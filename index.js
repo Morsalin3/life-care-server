aac3fa73bea7ba83b15e6fc66188a905d5996315
Morsalin3 <mdmorsalin3@gmail.com>
2022-11-11 12:30:47 +0600

    create service get & post route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000 ;
@@ -13,12 +13,40 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ojgvcnk.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+    try{
+        const serviceCollection = client.db('lifeCare').collection('services');
 
+        // get services
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        //get a serviec details
+        app.get('/details/:id', async(req, res)=>{
+            const id = req.params.id;
+            console.log(id)
+            const query = {_id: ObjectId(id)};
+            const details = await serviceCollection.findOne(query);
+            res.send(details)
+        });
+        // post a service on database
+        app.post('/service', async(req, res)=>{
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
+        
+    }
+    finally{
+
+    }
+}
+
+run().catch(error =>console.error(error));
 
 
 app.get('/', (req, res)=>{
